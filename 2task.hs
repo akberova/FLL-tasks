1d00d298eb5e58548b8c76f2c6bca5a6abcd43c4
masha <rybalkamasha64@gmail.com>
2021-10-14 16:12:47 +1000

    fix empty task

--- 2task.hs
+++ 2task.hs
@@ -1,1 +1,7 @@
-fibonacci 0 = 0
+step n | n<0 = -1
+       | n>0 = 1
+
+fibonacciNeg 0 = 0
+fibonacciNeg 1 = 1
+fibonacciNeg (-1) = (-1)
+fibonacciNeg n = fibonacciNeg (n - step n) + fibonacciNeg (n - 2*step n)
