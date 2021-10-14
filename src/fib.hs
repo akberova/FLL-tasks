424c963189601381fb5754ebe0adc27f431dca4b
FunnyWelder <slavakuzm@mail.ru>
2021-10-14 16:05:12 +1000

    add fibonacci acc

--- src/fib.hs
+++ src/fib.hs
@@ -3,3 +3,14 @@ fibonacci n | n == 0 = 0
             | n == 1 = 1
             | n > 0 = fibonacci (n - 1) + fibonacci (n - 2)
             | n < 0 = fibonacci (n + 2) - fibonacci (n + 1)
+
+
+
+fibonacci2 n | n == 0 = 0
+             | n == 1 = 1
+             | n == -1 = 1
+             | n > 0 = helper 1 1 n 
+	     | n < 0 = (-1)^(-n+1) * fibonacci2(-n)
+
+helper acc acc' i | i == 2 = acc
+                  | i > 2 = helper (acc + acc') acc (i - 1) 
