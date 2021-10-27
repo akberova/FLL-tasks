9714674fe31d33b873dc178ff954f477438e02f1
Alexey <alexeyvoytenko.2000@gmail.com>
2021-10-27 19:42:46 +1000

    5.hs

---
+++ 5.hs
@@ -0,0 +1,33 @@
+getProductCount :: ([Char], Integer, Integer) -> Integer
+getProductCount (_, a, _) = a
+
+getProductCost :: ([Char], Integer, Integer) -> Integer
+getProductCost (_, _, a) = a
+
+getTotalProductCost :: ([Char], Integer, Integer) -> Integer
+getTotalProductCost product = getProductCount(product)*getProductCost(product)
+
+getMostProfitableProductHelper :: (([Char], Integer, Integer), [([Char], Integer, Integer)]) -> ([Char], Integer, Integer)
+getMostProfitableProductHelper (currentMostProfitable, []) = currentMostProfitable
+getMostProfitableProductHelper (currentMostProfitable, rest) = 
+    if getTotalProductCost(currentMostProfitable) < getTotalProductCost(head(rest)) then
+        getMostProfitableProductHelper (head(rest), tail(rest))
+    else
+        getMostProfitableProductHelper (currentMostProfitable, tail(rest))
+
+
+getMostProfitableProduct :: [([Char], Integer, Integer)] -> ([Char], Integer, Integer)
+getMostProfitableProduct []       = error "This function works with non empty values"
+getMostProfitableProduct products = getMostProfitableProductHelper(("getMostProfitableProductStub", 0, 0), products)
+
+-- testing --
+assert :: Bool -> Bool
+assert False = error "Assertion failed"
+assert True  = True
+
+testNull   = getMostProfitableProduct([])       -- Should just throw error
+testSingle = getMostProfitableProduct([("Name", 10, 10)]) == ("Name", 10, 10) 
+-- TODO: can we do something like getMostProfitableProduct([n]) == n ?
+testMultipleFirstBetter   = getMostProfitableProduct([("First", 10, 20), ("Second", 4, 40)]) == ("First", 10, 20)
+testMultipleSecondBetter  = getMostProfitableProduct([("First", 10, 20), ("Second", 4, 60)]) == ("Second", 4, 60)
+
