e00bde0194ccb1e489b575871268ba58d38c5e2f
masha <rybalkamasha64@gmail.com>
2021-10-28 16:07:04 +1000

    add 12 task

---
+++ 12task.hs
@@ -0,0 +1,11 @@
+
+sum'n'count x | x < 0 = acc (-x)
+              | x>=0 = acc x
+                        where acc x = (sum, count)
+                                where
+                                    acc_s s 0 units = s + units
+                                    acc_s s x units = acc_s (s + div x 10) (div x 10) (mod units 10)
+                                    acc_c 0 = 0 
+                                    acc_c x = acc_c (div x 10) + 1
+                                    sum = acc_s 0 x x
+                                    count = acc_c x 
