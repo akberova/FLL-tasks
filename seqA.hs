1690d81d8824eef8698eac79319b67ba36b68e63
Vanek741 <44308306+Vanek741@users.noreply.github.com>
2021-11-11 15:53:35 +1000

    taskElevenComplete

---
+++ seqA.hs
@@ -0,0 +1,11 @@
+--–ешение с пары, не моЄ
+seqA n
+  | n >= 3 =
+    let 
+      helper acc1 acc2 acc3 2 = acc3
+      helper acc1 acc2 acc3 n = helper acc2 acc3 (acc3 + acc2 - 2 * acc1) (n - 1)
+     in helper 1 2 3 n
+  | n == 2 = 3
+  | n == 1 = 2
+  | n == 0 = 1
+main = putStrLn $ show $ seqA 301
