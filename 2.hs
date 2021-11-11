5b555bc3ca6912d91e94d4f020cb3a644fc3240f
Taki <62139377+Tanya-00@users.noreply.github.com>
2021-11-11 18:19:35 +1000

    First and second task

---
+++ 2.hs
@@ -0,0 +1,5 @@
+fibonacci n | n == 0 = 0
+            | n == 1 = 1
+            | n == (-1) = -1
+            | n > 0 = fibonacci(n - 1) + fibonacci(n - 2)
+            | n < 0 = fibonacci(n + 1) + fibonacci(n + 2)
