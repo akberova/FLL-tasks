0a08b5df579ad4562ad1253898da17db04c4e641
Vanek741 <44308306+Vanek741@users.noreply.github.com>
2021-11-11 16:12:13 +1000

    taskThreeComplete

---
+++ Fib2.hs
@@ -0,0 +1,6 @@
+fib n = helper 0 1 n
+  where helper curr prev n
+          | n == 0   = curr
+          | n > 0    = helper (curr+prev) curr (n-1)
+          | n < 0    = helper prev (curr-prev) (n+1)
+main = putStrLn $ show $ fib $ -4
