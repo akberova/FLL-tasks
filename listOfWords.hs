b6c936a069548706cbf90ec337b9efa362a3d1e9
Vanek741 <44308306+Vanek741@users.noreply.github.com>
2021-11-24 18:11:17 +1000

    taskSixComplete

---
+++ listOfWords.hs
@@ -0,0 +1,6 @@
+wordsWhen     :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
+main = print $ wordsWhen (==' ') "etot paren' byl iz tekh kto znal stackoverflow"
