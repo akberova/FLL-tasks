349db90f8c7a94db8b024c55f2875ed2903ef9b9
Vanek741 <44308306+Vanek741@users.noreply.github.com>
2021-11-25 19:09:43 +1000

    tasksFourteenAndFifteenComplete

---
+++ Printable.hs
@@ -0,0 +1,11 @@
+class Printable a where
+    toString :: a -> String
+
+instance Printable Bool where
+    toString True = "true"
+    toString False = "false"
+
+instance Printable () where
+    toString () = "unit type"
+
+main = putStrLn $ show $ toString ()
---
+++ ProPrintable.hs
@@ -0,0 +1,14 @@
+class Printable a where
+    toString :: a -> String
+
+instance Printable Bool where
+    toString True = "true"
+    toString False = "false"
+
+instance Printable () where
+    toString () = "unit type"
+
+instance (Printable a, Printable b) => Printable (a, b) where
+    toString pair = "(" ++ (toString $ fst pair) ++ "," ++ (toString $ snd pair) ++ ")"
+
+main = putStrLn $ show $ toString (False,True)
