1175594a47ee52aecc30a75f2391e7f2246cfddd
Sophia <sophia.kuznetsova.7@gmail.com>
2021-12-21 03:28:04 +1000

    Delete tasks_3.hs

--- tasks_3.hs
+++
@@ -1,9 +0,0 @@
-fbnc helper i j | helper == 0 = i
-                | otherwise = fbnc (helper-1) (i+j) i
-
-
-fibonacci n | n == 0 = 0
-            | n == 1 = 1
-            | n == (-1) = -1
-            | n > 0 = fbnc (n-1) 1 0
-            | otherwise = fbnc ((-n)-2) (-1) (-1)
