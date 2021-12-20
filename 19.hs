90fdbbcd980f46e92db3b1dcb2549b60091e6458
Alexey <alexeyvoytenko.2000@gmail.com>
2021-12-20 18:19:01 +1000

    Added 19 task

---
+++ 19.hs
@@ -0,0 +1,10 @@
+extractMaxElementFromList :: (Ord a) => [a] -> a
+extractMaxElementFromList [] = error "Non empty list required"
+extractMaxElementFromList list = helper (tail(list)) (head(list))
+    where 
+        helper [] curMax = curMax
+        helper remaining curMax = if head(remaining) > curMax then helper (tail(remaining)) (head(remaining)) else helper (tail(remaining)) curMax
+
+
+extractMaxElementFromSubLists list = 
+    extractMaxElementFromList (fmap extractMaxElementFromList list)
