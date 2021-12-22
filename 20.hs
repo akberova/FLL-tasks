b0eff565fbc67ca871fff33ec290d3d114512942
Alexey <alexeyvoytenko.2000@gmail.com>
2021-12-22 20:12:01 +1000

    20 task done

--- 20.hs
+++ 20.hs
@@ -11,9 +11,35 @@ f4 :: [a] -> [[a]]
 f4 list = map helper list
     where helper a = [a]
 
--- f5 // TODO
+count :: (Eq a) => [a] -> a -> Integer
+count list elem = helper list elem
+    where 
+        helper [] elem = 0
+        helper list elem = if head(list) == elem then
+                               1 + helper (tail(list)) elem
+                           else
+                               helper (tail(list)) elem
 
--- f6 :: (Num a) => [a] -> [a]
--- f6 list = 
+f5 :: (Eq a) => [a] -> [(a, Int)]
+f5 list = helper [] list
+    where
+        helper result [] = result
+        helper result remaining = helper (result ++ [flip (,) (length (filter (== (head remaining)) remaining)) (head remaining)]) 
+                                         (filter (/= (head remaining)) remaining)
 
--- f7
+genericF6_F7 :: (Num a) => (a -> a -> a) -> [a] -> [a]
+genericF6_F7 op list | length(list) <= 1 = error "List must have at least 2 elements" 
+                     | otherwise = helper (length(list) - 2) [] remaining a b
+                     where
+                     a = head(list)
+                     b = head(tail(list))
+                     remaining = tail(tail(list))
+                     helper toIterate result buf a b
+                         | toIterate == 0 = result ++ [op a b]
+                         | otherwise = helper (toIterate - 1) (result ++ [op a b]) (tail(buf)) b (head(buf))
+
+f6 :: (Num a) => [a] -> [a]
+f6 = genericF6_F7 (-) 
+
+f7 :: (Num a) => [a] -> [a]
+f7 = genericF6_F7 (+) 
