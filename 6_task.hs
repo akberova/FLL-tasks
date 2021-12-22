27ae967676eba675900f4b64ba50592b439e00d1
Sophia <sophia.kuznetsova.7@gmail.com>
2021-12-22 10:27:57 +1000

    add with words

---
+++ 6_task.hs
@@ -0,0 +1,7 @@
+-- не знаю, можно ли так делать, но я нашла words
+-- words :: String -> [String]
+-- создает массив строк из исходного, символы пробела служат разделителями
+
+main = print $ words "hello world i am here how are you"
+
+-- ["hello","world","i","am","here","how","are","you"]
