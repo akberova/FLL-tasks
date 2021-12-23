2535a6d47e6c2f4652d21a97c08eacbc506c383b
matsavx <55607228+matsavx@users.noreply.github.com>
2021-12-23 15:45:51 +1000

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,10 @@ import Task1
 import Task2
 import Task4
 import Task5
+import Task6
+import Task7
+import Task8
+import Task9
 
 showAns num ans = "\n" ++ show num ++ "  " ++ show ans ++ "\n"
 
@@ -12,7 +16,15 @@ showAns num ans = "\n" ++ show num ++ "  " ++ show ans ++ "\n"
 -- ans = showAns 3 ()
 goods = [("Toy Car", 200, 2), ("Robot", 400, 3)]
 -- ans = showAns 4 (goodsSumArr goods)
-ans = showAns 5 (profit goods)
+-- ans = showAns 5 (profit goods)
+wordsArr = "dsfh skdjhf skdj"
+-- ans = showAns 6 (wordsFromSentence wordsArr)
+sentencesArr = "sdfhjk.sdfkjhk.sdf"
+-- ans = showAns 7 (sentencesFromText (=='.') sentencesArr)
+digitArr = [1,2,6,5,9]
+-- ans = showAns 8 (digitSort digitArr)
+letterArr = ["I", "a", "g"]
+ans = showAns 9 (letterSort letterArr)
 
 main :: IO()
 main = putStr(ans)
