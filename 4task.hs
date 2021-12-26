290091f483fcbe679072181f32ceddcf52d9953f
masha <rybalkamasha64@gmail.com>
2021-12-27 00:06:51 +1000

    fix extra eol

--- 4task.hs
+++ 4task.hs
@@ -2,6 +2,5 @@ import Data.ByteString (count)
 
 countForOne (name, number, price) = number*price
 
-
 countForAll productList | null productList = 0
                         | otherwise = countForAll (init productList) + countForOne (last productList)
