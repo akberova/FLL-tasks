4d5e551ed469e1388f7608a93be6a57ed78ed9f8
masha <rybalkamasha64@gmail.com>
2021-12-27 00:05:47 +1000

    test

--- 4task.hs
+++ 4task.hs
@@ -2,5 +2,6 @@ import Data.ByteString (count)
 
 countForOne (name, number, price) = number*price
 
+
 countForAll productList | null productList = 0
                         | otherwise = countForAll (init productList) + countForOne (last productList)
