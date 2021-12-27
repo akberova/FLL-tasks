158bc592654e122aed0a8c88ea95487405ef43ce
masha <rybalkamasha64@gmail.com>
2021-12-28 00:04:49 +1000

    add 7 task

---
+++ 7task.hs
@@ -0,0 +1,13 @@
+import Distribution.Simple.Setup (CleanFlags(cleanVerbosity))
+res = []
+acc = ""
+
+separateHelper :: [Char] -> [[Char]] -> [Char] -> [[Char]]
+separateHelper text res acc | null text = res ++ [acc]
+                            | last text == last "." = cleaner acc
+                            | otherwise = separateHelper (init text) res ((last text) : acc)
+                            where cleaner acc | acc == "" = separateHelper (init text) res ""
+                                              | otherwise = separateHelper (init text) (res++[acc]) ""
+
+separate :: [Char] -> [[Char]]
+separate text = separateHelper text res acc
