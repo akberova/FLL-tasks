1f8a2226fe43f639f6ea96271d4576be070a3c02
ElerGard <norfick@mail.ru>
2022-01-01 19:59:20 +1000

    finally finish 6 and 7 tasks

--- 6-7.hs
+++ 6-7.hs
@@ -1,22 +1,32 @@
-helper :: Char -> String -> String -> [String] -> [String]
-helper elem str word result | null (tail str) && head str /= ' ' = result
-                            | null (tail str) && head str == ' ' = secondHelper elem "" word result
-                            | elem == ' ' = secondHelper elem str word result
-                            | otherwise = helper (head str) (tail str) (word ++ [elem]) result
+helper :: Char -> String -> String -> [String] -> Char -> [String]
+helper elem str word result flag | null (tail str) = if elem == flag
+                                                        then if null word then lastElem (head str) [] result flag else lastElem (head str) [] (result ++ [word]) flag
+                                                        else if null word then lastElem (head str) [elem] result flag else lastElem (head str) (word ++ [elem]) result flag
+                                 | elem == flag = secondHelper elem str word result flag
+                                 | otherwise = helper (head str) (tail str) (word ++ [elem]) result flag
 
-secondHelper :: Char -> String -> String -> [String] -> [String]
-secondHelper elem "" [] result = result 
-secondHelper elem "" word result = if elem == ' '
-                                    then result ++ [word]
-                                    else result ++ [word ++ [elem]]
-secondHelper elem str [] result = helper (head str) (tail str) [] result
-secondHelper elem str word result = helper (head str) (tail str) [] (result ++ [word])
+secondHelper :: Char -> String -> String -> [String] -> Char -> [String]
+secondHelper _ str word result flag | head str == flag && tail str /= [] = secondHelper flag (tail str) word result flag
+                                    | null (tail str) = lastElem (head str) [] (result ++ [word]) flag
+                                    | null word = helper (head str) (tail str) [] result flag
+                                    | otherwise = helper (head str) (tail str) [] (result ++ [word]) flag
 
+
+lastElem :: Char -> String -> [String] -> Char -> [String]
+lastElem elem word result flag
+  | null word = if elem == flag then result else result ++ [[elem]]
+  | elem == flag = result ++ [word]
+  | otherwise = result
 
 toWords :: String -> [String]
 toWords "" = []
 toWords " " = []
-toWords a = helper (head a) (tail a) [] []
+toWords a = if null (tail a) then [a] else helper (head a) (tail a) [] [] ' '
+
+toSentences :: String -> [String]
+toSentences "" = []
+toSentences "." = []
+toSentences a = if null (tail a) then [a] else helper (head a) (tail a) [] [] '.'
 
 a :: String
 a = " Not" -- empty because there is no space after the word
