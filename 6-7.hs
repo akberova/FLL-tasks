a6deda9fadf3a804d7903307e541712c9ecaca69
ElerGard <norfick@mail.ru>
2022-01-01 18:20:33 +1000

    fixed some problems in 6 task

--- 6-7.hs
+++ 6-7.hs
@@ -1,13 +1,17 @@
-
 helper :: Char -> String -> String -> [String] -> [String]
-helper elem str word result | null (tail str) = secondHelper elem str word result
-                            | elem == ' ' = helper (head str) (tail str) [] (result ++ [word])
+helper elem str word result | null (tail str) && head str /= ' ' = result
+                            | null (tail str) && head str == ' ' = secondHelper elem "" word result
+                            | elem == ' ' = secondHelper elem str word result
                             | otherwise = helper (head str) (tail str) (word ++ [elem]) result
 
 secondHelper :: Char -> String -> String -> [String] -> [String]
-secondHelper elem str word result = if head str /= ' '
-                                    then result
+secondHelper elem "" [] result = result 
+secondHelper elem "" word result = if elem == ' '
+                                    then result ++ [word]
                                     else result ++ [word ++ [elem]]
+secondHelper elem str [] result = helper (head str) (tail str) [] result
+secondHelper elem str word result = helper (head str) (tail str) [] (result ++ [word])
+
 
 toWords :: String -> [String]
 toWords "" = []
@@ -17,7 +21,7 @@ toWords a = helper (head a) (tail a) [] []
 a :: String
 a = " Not" -- empty because there is no space after the word
 b = "Nat" -- same
-c = "Nat " -- correct
+c = "   Nat    " -- correct
 d = "Hello World" -- only ["Hello"]
 e = "Hello world " -- correct
 
