540269ec70c561c43312bf459184013ade9e217c
masha <rybalkamasha64@gmail.com>
2022-01-08 22:26:21 +1000

    add some fool check

--- 9task.hs
+++ 9task.hs
@@ -3,6 +3,7 @@ acc = []
 
 sort :: [[Char]] -> [[Char]] -> [[Char]]
 sort acc wordList | null wordList = acc
+                  | head wordList == "" = sort acc (tail wordList)
                   | null acc = sort [head wordList] (tail wordList)
                   | (head wordList > head acc) && (head wordList > last acc) = sort (acc ++ [head wordList]) (tail wordList)
                   | head wordList < head acc = sort (head wordList : acc) (tail wordList)
