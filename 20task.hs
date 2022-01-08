65f228435e7ecdcf3590352aeae9b8a74e4aac04
masha <rybalkamasha64@gmail.com>
2022-01-09 02:48:35 +1000

    add new functions

--- 20task.hs
+++ 20task.hs
@@ -1,3 +1,4 @@
+import qualified Data.List as List
 add num = map (+num)
 
 mul num = map (*num)
@@ -5,3 +6,12 @@ mul num = map (*num)
 pair :: b1 -> [a] -> [(a, b1)]
 pair num = map (makePair num)
                 where makePair num a = (a, num)
+
+list = map makeList
+                where makeList a = [a]
+
+
+listOfPairs list = map makeListPairs (List.nub list)
+                where
+                    makeListPairs a = [(a, length (filter (==a) list))]
+
