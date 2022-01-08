298c425741640786596eb0656825a1a307101fda
masha <rybalkamasha64@gmail.com>
2022-01-08 22:23:54 +1000

    add 9 task

---
+++ 9task.hs
@@ -0,0 +1,16 @@
+acc :: [[Char]]
+acc = []
+
+sort :: [[Char]] -> [[Char]] -> [[Char]]
+sort acc wordList | null wordList = acc
+                  | null acc = sort [head wordList] (tail wordList)
+                  | (head wordList > head acc) && (head wordList > last acc) = sort (acc ++ [head wordList]) (tail wordList)
+                  | head wordList < head acc = sort (head wordList : acc) (tail wordList)
+                  | otherwise = findPos wordList (tail acc) leftPart
+                  where
+                      leftPart = [head acc]
+                      findPos numberList acc leftPart | not (null numberList) && (head numberList < head acc) = sort ((leftPart ++ [head numberList]) ++ acc) (tail numberList)
+                                                      | otherwise = findPos numberList (tail acc) (leftPart ++ [head acc])
+
+sortList :: [[Char]] -> [[Char]]
+sortList = sort acc
