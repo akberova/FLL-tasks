d0814fba75982ff4873e5e44dd87f295a9e15261
masha <rybalkamasha64@gmail.com>
2022-01-08 22:48:18 +1000

    add 10 task

---
+++ 10task.hs
@@ -0,0 +1,20 @@
+acc :: [([Char], Int)]
+acc = []
+
+sort :: [([Char], Int)] -> [([Char], Int)] -> [([Char], Int)]
+sort acc studentList | null studentList = acc
+                     | checkEr (head studentList) = sort acc (tail studentList)
+                     | null acc = sort [head studentList] (tail studentList)
+                     | moreThanAcc(head studentList) (head acc) && moreThanAcc(head studentList) (last acc) = sort (acc ++ [head studentList]) (tail studentList)
+                     | lessThanAcc (head studentList) (head acc) = sort (head studentList : acc) (tail studentList)
+                     | otherwise = findPos studentList (tail acc) leftPart
+                     where
+                         checkEr (name, avg) = avg == 0 || name == "" || avg < 0
+                         moreThanAcc (name, avg) (nameAcc, avgAcc) = avg > avgAcc
+                         lessThanAcc (name, avg) (nameAcc, avgAcc) = avg < avgAcc
+                         leftPart = [head acc]
+                         findPos studentList acc leftPart | not (null studentList) && lessThanAcc (head studentList) (head acc) = sort ((leftPart ++ [head studentList]) ++ acc) (tail studentList)
+                                                          | otherwise = findPos studentList (tail acc) (leftPart ++ [head acc])
+
+sortList :: [([Char], Int)] -> [([Char], Int)]
+sortList = sort acc
