907580edfd726c371f9e907920ac0bf9a2da48e5
DieNice <DailyPlanning@bk.ru>
2022-01-09 16:47:55 +1000

    Update final.hs
    Правки

--- final.hs
+++ final.hs
@@ -7,9 +7,14 @@ excludePos list = filter (<= 0) list
 lengthNonpos :: (Num a, Ord a) => [a] -> Int
 lengthNonpos list = length (excludePos list)
 
+-- minOfNonPosThatMoreThanElem [1,2,3,5,6] 1
+*** Exception: Prelude.minimum: empty list
 minOfNonPosThatMoreThanElem :: (Num a, Ord a) => [a] -> a -> a
 minOfNonPosThatMoreThanElem list x = minimum (filter (> x) (excludePos list))
 
+-- Вычисление ПРОИВЕДЕНИЙ нечетных элементов списка, а не произведения
 productOfOdd :: (Integral a,Num a) => [a] -> a
 productOfOdd list = foldl1 (*) (filter odd list)
+
+-- Нет сортировки
 
