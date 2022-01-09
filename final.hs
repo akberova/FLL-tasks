802401d90fcef6fc920faf31a52b702ca002e6c3
DieNice <DailyPlanning@bk.ru>
2022-01-09 16:08:27 +1000

    Update final.hs
    Правки

--- final.hs
+++ final.hs
@@ -15,6 +15,8 @@ subset a b = if (repeated a == True) then error "There can be no duplicate eleme
     if (repeated b == True) then error "There can be no duplicate elements in the set" else all (\x -> elem x b) a
 
 --second
+-- На этих входных данных выдаёт False
+-- equal [1,2,3,4] [4,3,2,1]
 equal :: (Eq a) => [a] -> [a] -> Bool
 equal [] [] =  True 
 equal (x:xs) (y:ys) = if (repeated xs == True) then error "There can be no duplicate elements in the set" else 
@@ -47,3 +49,5 @@ maxElem (y:ys) = if (repeated ys == True) then error "There can be no duplicate 
     foldr (\x (m) -> if (x==m) then (m) 
                                   else if x > m then (x)
                                        else (m)) (y) ys
+-- senevth
+https://ru.wikipedia.org/wiki/%D0%9E%D1%82%D0%BD%D0%BE%D1%88%D0%B5%D0%BD%D0%B8%D0%B5_%D1%8D%D0%BA%D0%B2%D0%B8%D0%B2%D0%B0%D0%BB%D0%B5%D0%BD%D1%82%D0%BD%D0%BE%D1%81%D1%82%D0%B8
